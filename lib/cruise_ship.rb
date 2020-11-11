e7996170329727ab7bfbcbd0d716f23a1b17fabd
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-11 23:29:23 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,8 +7,11 @@
 # }
 
 def select_winner(passengers)
- winner = ""
+winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
-end 
+  end
+end
+ 
+winner
